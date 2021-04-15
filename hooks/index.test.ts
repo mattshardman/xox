70920bf991d59c9c7d64f477f4f69300a3908075
Matt Hardman <mattshardman@gmail.com>
2021-04-15 19:29:59 +0100

    feat: :sparkles: implement create board

--- hooks/index.test.ts
+++ hooks/index.test.ts
@@ -1,7 +1,13 @@
+import { renderHook, act } from "@testing-library/react-hooks";
 import { useGame } from ".";
 
 test("it works", () => {
-  const res = useGame();
+  const { result } = renderHook(() => useGame(3));
+  const { result: r2 } = renderHook(() => useGame(7));
 
-  expect(res).toBe(true);
+  expect(result.current.board?.length).toBe(3);
+  expect(result.current.board?.[0].length).toBe(3);
+
+  expect(r2.current.board?.length).toBe(7);
+  expect(r2.current.board?.[0].length).toBe(7);
 });
--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,25 @@
-export const useGame = () => {
-    return true;
-}
+import React from "react";
+
+// [[null, null, null], [null, null, null], [null, null, null]]
+
+type Board = Array<Array<CellOption>>;
+
+type CellOption = "X" | "O" | null;
+
+const createBoard = (n: number) =>
+  [...Array(n)].map(() => [...Array(n)].map(() => null));
+
+export const useGame = (n: number) => {
+  const [board, setBoard] = React.useState<Board>();
+
+  React.useEffect(() => {
+    const newBoard = createBoard(n);
+    setBoard(newBoard);
+  }, [n]);
+
+  return {
+    board,
+    playMove: () => {},
+    reset: () => {},
+  };
+};
